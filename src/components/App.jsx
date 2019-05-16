f29761c2216ea378edcbb1998a6c841a44badbe6
thule <lunthu@gmail.com>
2019-05-16 10:17:02 +0700

    add avatar with faker

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import faker from 'faker';
 
 class App extends Component {
   render(){
     return(
-      <div>
-        <h1>Hello React-Redux</h1>
+      <div className="ui container comment">
+        <div className="comment">
+          <a href="/" className="avatar">
+            <img alt="avatar" src={faker.image.avatar()} />
+          </a>
+          <div className="content">
+            <a href="/" className="author">
+              <h4>thu</h4>
+            </a>
+            <div className="metadata">
+              <span className="date">
+                <p>Today at 6:00 pm</p>
+              </span>
+            </div>
+            <div className="text">
+              <p>Nice blog</p>
+            </div>
+          </div>
+        </div>
       </div>
       )
   }
