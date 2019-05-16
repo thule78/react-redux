c2a9659453c9124e5297046d38b7257b6160128c
thule <lunthu@gmail.com>
2019-05-16 09:55:42 +0700

    add semantic UI style

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App'
 
-import '../assets/stylesheets/application.scss';
-
   ReactDOM.render(<App />, document.getElementById('root'));
 
