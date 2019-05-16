a78ab631bdf42a815d820f9576708e9d8c14ad46
thule <lunthu@gmail.com>
2019-05-16 09:42:09 +0700

    App.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './components/App'
+
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+  ReactDOM.render(<App />, document.getElementById('root'));
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
-}
