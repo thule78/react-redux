ccf28619a60c746b7110f7e5a14f69ebc823b670
thule <lunthu@gmail.com>
2019-05-18 12:30:13 +0700

    componentDidMount

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom';
 
 function Nav (){
   return (
-    <div className="ui secondary  menu">
+    <div className="">
       <Link className="item" to="/">Home</Link>
       <Link className="item" to="/commentbox">Comment Box</Link>
       <Link className="item" to="/seasondisplay">Season Display</Link>
--- src/components/SeasonDisplay.jsx
+++ src/components/SeasonDisplay.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Season from './Season';
 
-function SeasonDisplay(){
-  window.navigator.geolocation.getCurrentPosition (
+
+  /*window.navigator.geolocation.getCurrentPosition (
     position => console.log(position),
     err => console.log(err)
-    );
-  return(
-    <Season />
-    )
-}
+    );*/
+
+  class SeasonDisplay extends Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        lat: null,
+        errMessage: ''
+      };
+
+    }
+
+    componentDidMount (){
+      window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errMessage: err.Message})
+      )}
+
+
+    render(){
+      if (this.state.errMessage && !this.state.lat) {
+        return (
+          <div>{this.state.errMessage}</div>
+          )}
+
+      if (!this.state.errMessage && this.state.la){
+        return (
+          <div>{this.state.lat}</div>
+          )}
+
+      return (
+        <div><p>Loading...</p></div>
+        )}
+  }
 export default SeasonDisplay
