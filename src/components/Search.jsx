3be4380372b6b73401cef549bf66a6667c4ea2af
thule <lunthu@gmail.com>
2019-05-22 12:51:50 +0700

    props error, link between search and searchbar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,11 +4,14 @@ import SearchBar from './SearchBar';
 import SearchList from './SearchList';
 
 class Search extends Component{
+  onSearchSubmit(term){
+    console.log(term);
+  }
   render(){
     return(
       <div>
         <h3>search from API</h3>
-        <SearchBar />
+        <SearchBar onThisSubmitChange={this.onSearchSubmit}/>
         <SearchList />
       </div>
       )
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component{
+  constructor(){
+    super();
+    this.state = {
+      term: ''
+    }
+    this.onInputChange = this.onInputChange.bind(this)
+  }
+  onInputChange(event){
+    this.setState({term: event.target.value})
+  }
+
+  onSubmitChange(e){
+    e.preventDefault();
+    this.props.onThisSubmitChange(this.state.term)
+  }
   render(){
     return(
       <div>
-        <form>
-         <input type="text" />
+        <form onSubmit={this.onSubmitChange}>
+         <input type="text" value={this.state.term} onChange={this.onInputChange}/>
         </form>
       </div>
       )
