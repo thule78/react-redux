d34de2c09e42d61a7af3ff3e1c9e61596acc72af
thule <lunthu@gmail.com>
2019-05-24 10:08:17 +0700

    fixed promise .then to this setSate

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,15 +6,32 @@ import SearchBar from './SearchBar';
 import SearchList from './SearchList';
 
 class Search extends Component{
-  onSearchSubmit(term){
-    console.log(term);
+  constructor(){
+    super();
+    this.state = {
+      images:[]
+    }
+
+    this.onSearchSubmit = this.onSearchSubmit.bind(this)
   }
+
+
+  onSearchSubmit(term){
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: {query: term},
+      headers: {Authorization: 'Client-ID 95d6d4fc153470f7f5d9361417bb8f07e5da30dd2a57f413b112f394618505f1'}
+    })
+    .then(response => {this.setState({images: response.data.results});
+  });
+  }
+
   render(){
     return(
       <div>
         <h3>search from API</h3>
         <SearchBar onThisSubmitChange={this.onSearchSubmit}/>
         <SearchList />
+        Found:{this.state.images.length} images
       </div>
       )
   }
